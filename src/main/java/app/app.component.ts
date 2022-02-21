9923dac4616114a0021f012ff5e0407e01b31611
Melanie2911 <95179280+Melanie2911@users.noreply.github.com>
2022-02-21 21:22:38 +0100

    Create app.component.ts

---
+++ src/main/java/app/app.component.ts
@@ -0,0 +1,12 @@
+@Component({
+  selector: 'app-root',
+  template: `<nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="/">Spring Security Oauth - Authorization Code</a>
+      </div>
+    </div>
+  </nav>
+  <router-outlet></router-outlet>`
+})
+export class AppComponent { }
