d9b235300163db4376101a7b8a551a7a6d0a728c
Melanie2911 <95179280+Melanie2911@users.noreply.github.com>
2022-02-21 21:23:26 +0100

    Add files via upload

---
+++ src/main/java/app/app.module.ts
@@ -0,0 +1,16 @@
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FooComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+     { path: '', component: HomeComponent, pathMatch: 'full' }], {onSameUrlNavigation: 'reload'})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
